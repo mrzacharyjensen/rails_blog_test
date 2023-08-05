e546be84d6579ea5901a667f8295a9ad98f830b4
Zachary Jensen <mrzacharyjensen@gmail.com>
2023-08-06 11:31:02 +1200

    Tidied routing using article_path and link_to

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
+  resources :articles
+
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
 end
